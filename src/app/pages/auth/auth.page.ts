8ae639deef6e1f3d2e09bc59f6dfd9a0422e5690
MariaPas235 <151913868+MariaPas235@users.noreply.github.com>
2025-02-18 19:58:52 +0100

    inicio sesion

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormControl,
@@ -11,16 +11,25 @@ import { IonContent, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { CustomInputComponent } from 'src/app/shared/components/custom-input/custom-input.component';
 import { addIcons } from 'ionicons';
-import { lockClosedOutline, mailOutline, logInOutline, personAddOutline } from 'ionicons/icons';
+import {
+  lockClosedOutline,
+  mailOutline,
+  personAddOutline,
+  logInOutline,
+} from 'ionicons/icons';
 import { IonButton } from '@ionic/angular/standalone';
-import { LogoComponent } from "../../shared/components/logo/logo.component";
+import { LogoComponent } from 'src/app/shared/components/logo/logo.component';
+import { RouterLink } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonIcon, 
+  imports: [
+    IonIcon,
     HeaderComponent,
     IonContent,
     CommonModule,
@@ -28,21 +37,33 @@ import { LogoComponent } from "../../shared/components/logo/logo.component";
     CustomInputComponent,
     ReactiveFormsModule,
     IonButton,
-    LogoComponent
-],
+    LogoComponent,
+    RouterLink,
+  ],
 })
 export class AuthPage implements OnInit {
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
+
+  firebaseService = inject(FirebaseService);
+
+  
   constructor() {
-    addIcons({logInOutline,personAddOutline,mailOutline,lockClosedOutline});
+    addIcons({
+      mailOutline,
+      lockClosedOutline,
+      personAddOutline,
+      logInOutline,
+    });
   }
 
   ngOnInit() {}
 
   submit() {
-    console.log(this.form.value);
+    this.firebaseService.signIn(this.form.value as User).then(res =>
+      console.log(res)
+    );
   }
 }
