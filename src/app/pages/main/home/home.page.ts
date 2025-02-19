1f701c47bb98b174af71e3600f666aa1fc2a945c
WaShIsTyLee <lopezsolanojuanjesus@gmail.com>
2025-02-19 19:56:36 +0100

    hasta punto18 tuto

---
+++ src/app/pages/main/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { IonButton, IonContent } from '@ionic/angular/standalone';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonButton, HeaderComponent, IonContent],
+})
+export class HomePage implements OnInit {
+  firebaseService = inject(FirebaseService);
+  utilsService = inject(UtilsService);
+  constructor() {}
+
+  ngOnInit() {}
+
+  async signOut() {
+    this.firebaseService.signOut().then(() => {
+      this.utilsService.routerLink('/auth');
+    });
+  }
+}
