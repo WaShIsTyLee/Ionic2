5d0dacb1e658934f538b5c4fc7669c8a46c05a9b
= <=>
2025-02-24 10:58:54 +0100

    hasta el 30

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,19 @@
 import { CanActivateFn } from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+import { UtilsService } from '../services/utils.service';
+import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = async (route, state) => {
+  const firebaseService = inject(FirebaseService);
+  const utilsService = inject(UtilsService);
+
+  const isAuthenticated = await firebaseService.isAuthenticated();
+  const localStorageUser = localStorage.getItem('user');
+
+  if (isAuthenticated && localStorageUser) {
+    return true;
+  } else {
+    firebaseService.signOut();
+    return utilsService.urlTree('/auth');
+  }
 };
